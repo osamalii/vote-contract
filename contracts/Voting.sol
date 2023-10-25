b689e975a10096329a6319242f76b3b046402019
osamalii <ousssama.aouli@gmail.com>
2023-10-25 12:55:26 +0200

    add session states

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,78 +5,88 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract  Voting is Ownable(msg.sender) {
+
     struct Voter {
         bool isRegistered;
         bool hasVoted;
         uint votedProposalId;
     }
+    
     struct Proposal {
         string description;
         uint256 voteCount;
     }
-    enum etat {
-        register,
-        closeregister,
-        vote,
-        closevote
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+	    ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+       	VotingSessionStarted,
+	    VotingSessionEnded,
+	    VotesTallied
     }
+
     struct Session {
-        etat etat;
+        WorkflowStatus status;
         bool isPublicProposal;
         Proposal[] lesProposition;
     }
 
     Proposal newPorosal;
-    Voter currentVote = Voter(false,false,0);
+    Voter currentVote = Voter(false, false, 0);
     Voting.Session currentSession;
     Proposal winner;
-    mapping (address =>Voter) public whitelist;
+    mapping (address => Voter) public whitelist;
     
     event newProposition(Proposal);
-    event etatSession(Session);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
     
     modifier isWhiteList {
         require( whitelist[msg.sender].isRegistered ,"is not whitelisted");
         _;
     }
-    function addVoter(address _address)public onlyOwner() {
+
+    function addVoter(address _address) public onlyOwner() {
         require(!whitelist[_address].isRegistered);
-        whitelist[_address]=Voter(true,false,0);
+        whitelist[_address] = Voter(true,false,0);
+        emit WorkflowStatusChange(currentSession.status, WorkflowStatus.RegisteringVoters);
+        currentSession.status = WorkflowStatus.RegisteringVoters;
+
     }
 
     function startSession(bool _publicProposal) public onlyOwner {
-        whitelist[owner()].isRegistered=true;
-        currentSession.etat=etat.register;
-        currentSession.isPublicProposal=_publicProposal;
-        emit etatSession(currentSession);
+        whitelist[owner()].isRegistered = true;
+        emit WorkflowStatusChange(currentSession.status, WorkflowStatus.ProposalsRegistrationStarted);
+        currentSession.status = WorkflowStatus.ProposalsRegistrationStarted;
+        currentSession.isPublicProposal = _publicProposal;
     }
 
     function closeProposition() public onlyOwner {
-        require(currentSession.etat==etat.register,"Session need be in register status before");
-        currentSession.etat = etat.closeregister;
-        emit etatSession(currentSession);
+        require(currentSession.status==WorkflowStatus.ProposalsRegistrationStarted,"Session need be in register status before");
+        emit WorkflowStatusChange(currentSession.status, WorkflowStatus.ProposalsRegistrationEnded);
+        currentSession.status = WorkflowStatus.ProposalsRegistrationEnded;
     }
     function openVote() public onlyOwner  {
-        require(currentSession.etat==etat.closeregister,"Session need be in closeregister status before");
-        currentSession.etat = etat.vote;
-        emit etatSession(currentSession);
+        require(currentSession.status == WorkflowStatus.ProposalsRegistrationEnded,"Session need be in closeregister status before");
+        emit WorkflowStatusChange(currentSession.status, WorkflowStatus.VotingSessionStarted);     
+        currentSession.status = WorkflowStatus.VotingSessionStarted;
     }
     function closeVote() public onlyOwner  {
-        require(currentSession.etat==etat.vote,"Session need be in vote status before");
-        currentSession.etat = etat.closevote;
-        emit etatSession(currentSession);
+        require(currentSession.status == WorkflowStatus.VotingSessionStarted,"Session need be in vote status before");
+        emit WorkflowStatusChange(currentSession.status, WorkflowStatus.VotingSessionEnded);
+        currentSession.status = WorkflowStatus.VotingSessionEnded;
     }
 
-    function setVote(uint _ProposalId) public{
-        require(whitelist[msg.sender].isRegistered&& currentSession.etat==etat.vote &&!currentVote.hasVoted );
-        currentSession.lesProposition[_ProposalId].voteCount+=1;
-        currentVote.hasVoted=true;
+    function setVote(uint _ProposalId) isWhiteList public{
+        require(currentSession.status == WorkflowStatus.VotingSessionStarted && !currentVote.hasVoted);
+        currentSession.lesProposition[_ProposalId].voteCount += 1;
+        currentVote.hasVoted = true;
         
     }
     function proposer(string calldata _description ) isWhiteList public {
-        if(currentSession.isPublicProposal || owner()==msg.sender){
+        if(currentSession.isPublicProposal || owner() == msg.sender){
             require(
-                currentSession.etat == etat.register
+                currentSession.status == WorkflowStatus.ProposalsRegistrationStarted
             );
             newPorosal = Proposal(_description, 0);
             currentSession.lesProposition.push(newPorosal);
@@ -84,7 +94,7 @@ contract  Voting is Ownable(msg.sender) {
         }
     }
 
-    function contabiliserVote()public onlyOwner {
+    function contabiliserVote() public onlyOwner {
         uint i=1;
         uint max =currentSession.lesProposition[0].voteCount;
         winner=currentSession.lesProposition[0];
@@ -96,10 +106,10 @@ contract  Voting is Ownable(msg.sender) {
         }
         
     }
-     function getSession()public view returns (Voting.Session memory){
+     function getSession() public view returns (Voting.Session memory){
         return currentSession;
     }
-    function getWinner()public view  returns(Proposal memory){
+    function getWinner() public view  returns(Proposal memory){
         return winner;
     }
 }
